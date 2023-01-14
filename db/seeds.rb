316754efd0d0a8fce702afef91d23214e0f65bd3
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2023-01-14 17:33:30 -0500

    edit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,46 +16,51 @@ users.each do |user|
   User.create(user)
 end
 
-# #Questions
-# questions = [
-#   {
-#     user_id: 1,
-#     question_1: "Is there any event affecting your mood today?",
-#     answer_1: "Yes",
-#     question_2: "How does this event affect your thoughts and behavior?",
-#     answer_2: "Positive",
-#     question_3: "Mood Scale",
-#     answer_3: "5",
-#   },
-#   {
-#     user_id: 2,
-#     question_1: "Is there any event affecting your mood today?",
-#     answer_1: "No",
-#     question_2: "How does this event affect your thoughts and behavior?",
-#     answer_2: "Negative",
-#     question_3: "Mood Scale",
-#     answer_3: "1",
-#   },
-# ]
+#Questions
+questions = [
+  {
+    user_id: 1,
+    question_1: "Is there any event affecting your mood today?",
+    answer_1: "Yes",
+    question_2: "How does this event affect your thoughts and behavior?",
+    answer_2: "Positive",
+    question_3: "Mood Scale",
+    answer_3: "5",
+  },
+  {
+    user_id: 1,
+    question_1: "Is there any event affecting your mood today?",
+    answer_1: "No",
+    question_2: "How does this event affect your thoughts and behavior?",
+    answer_2: "Negative",
+    question_3: "Mood Scale",
+    answer_3: "1",
+  },
+]
 
-# questions.each do |question|
-#   Question.create(question)
-# end
+questions.each do |question|
+  Question.create(question)
+end
 
-# results = Result.create([
-#   {
-#     user_id: 1,
-#     emoji: "😁",
-#     mood: "happy",
-#   },
-#   {
-#     user_id: 2,
-#     emoji: "🙂",
-#     mood: "pleased",
-#   },
-# ])
+results = Result.create([
+  {
+    user_id: 1,
+    emoji: "😁",
+    mood: "happy",
+  },
+  {
+    user_id: 1,
+    emoji: "😊",
+    mood: "pleased",
+  },
+  {
+    user_id: 1,
+    emoji: "😣",
+    mood: "Frustrated",
+  },
+])
 
-# vitals = Vital.create([
-#   { user_id: 1, heart_rate: 80.0, wrist_temperature: 98.6 },
-#   { user_id: 2, heart_rate: 72.0, wrist_temperature: 97.8 },
-# ])
+vitals = Vital.create([
+  { user_id: 1, heart_rate: 80.0, wrist_temperature: 98.6 },
+  { user_id: 2, heart_rate: 72.0, wrist_temperature: 97.8 },
+])
